0f6eabd5eb4da9efaa63a5c72c699d964e6f8faa
Ayush K M <kmayushkm@gmail.com>
2022-04-21 19:35:47 +0530

    I dont know why

--- include/applets.hpp
+++ include/applets.hpp
@@ -5,4 +5,9 @@
 
 int echo_main(int argc, char* argv[]);
 
+void applets_call(int argc, char* argv[]);
+void applets_as_main(int argc, char* argv[]);
+void usage();
+void version();
+
 #endif	/*APPLETS*/
